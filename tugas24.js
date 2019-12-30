6830557177978f5960dcca848265394431da73cd
STR <saturasstotenkopf@gmail.com>
2019-12-30 19:14:01 +0700

    Tugas 24 JS
    Mengumpulkan Tugas 24 JS

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+function tugas(){
+  var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+  console.log("Sebelumnya: ",angka);
+  angka.sort();
+  console.log("Ascending: ",angka);
+  angka.reverse(angka);
+  console.log("Descending: ",angka);
+}
+tugas();
